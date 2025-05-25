329467497b21b7e09219ad276e134c3dd45eb535
Syeda Maham Amjad <zohameer96@gmail.com>
2025-05-25 19:07:24 +0500

    first commit

---
+++ alerts.js
@@ -0,0 +1,17 @@
+    // Write a script to greet your website visitor using JS alert
+// box.
+alert("welcome to my first js program!");
+
+
+// Write a script to display following message on your web page:
+alert("Error! please enter a valid password");
+
+// Write a script to display following message on your web
+// page: (Hint : Use line break)
+
+alert("Welcome to JS land \n Happy Coding!")
+
+// Write a script to display following messages in sequence:
+alert("Welcome to JS land")
+alert("Happy Coding!")
+
---
+++ stringvariables.js
@@ -0,0 +1,49 @@
+        // Declare a variable called username.
+        var username;
+//         Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+        var myname="syeda maham amjad";
+        // Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+var message;
+ message="Hello World";
+alert(message);
+ 
+// Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+
+var studentname="maham";
+let studentID = "S12345";
+let course = "modern web app development";
+alert("student's name : "+studentname);
+alert("Student's ID : " +studentID);
+alert("Course : " + course);
+
+// Write a script to display the following alert using one JS
+// variable:
+alert("Pizza \n Pizz \n Piz \n Pi\n P")
+// Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+
+var email="zohameer96@gmail.com";
+alert("My email is address is : " + email);
+
+// Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+
+var book="A Smarter Way To Learn Javascript.";
+alert("I am trying to learn from the Book : " + book);
+
+// 8. Write a script to display this in browser through JS
+document.write("<h1>Yah! i can write HTML content through javascript</h1>");
+
+// Store following string in a variable and show in alert and
+// browser through JS
+
+var sym="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(sym);
